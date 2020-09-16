3fa0ed9cce21df67461c7183e51c8149e714e7de
Daniel Xu <dxu@dxuuu.xyz>
2020-09-16 15:10:33 -0700

    x.py: Add shell command

--- x.py
+++ x.py
@@ -14,18 +14,41 @@ def sh(cmd):
 
 
 def cmd_build(args):
-    sh('podman pull dxuu/btrfs-fuzz')
-    sh('cargo build')
+    if args.local:
+        sh('podman build -t btrfs-fuzz .')
+    else:
+        sh('podman pull dxuu/btrfs-fuzz')
 
 
 def cmd_run(args):
     print("Starting btrfs-fuzz")
+    print("TODO")
+
+
+def cmd_shell(args):
+    c = ['podman run']
+    c.append('-it')
+    c.append('--privileged')
+
+    if args.state_dir:
+        c.append(f"-v {args.state_dir}:/state")
+
+    if args.local:
+        c.append('localhost/btrfs-fuzz')
+    else:
+        c.append('dxuu/btrfs-fuzz')
+
+    sh(' '.join(c))
 
 
 def main():
     parser = argparse.ArgumentParser(
             prog='x',
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+            '--local',
+            action='store_true',
+            help='Use local docker image')
     parser.set_defaults(func=lambda _: parser.print_help())
 
     subparsers = parser.add_subparsers(help='subcommands')
@@ -35,6 +58,14 @@ def main():
 
     run = subparsers.add_parser('run', help='run fuzzer')
     run.set_defaults(func=cmd_run)
+
+    shell = subparsers.add_parser('shell', help='start shell in VM')
+    shell.add_argument(
+            '-s',
+            '--state-dir',
+            type=str,
+            help='Shared state directory between host and VM, mounted in VM at /state')
+    shell.set_defaults(func=cmd_shell)
 
     help = subparsers.add_parser(
             'help',
