d75477db3d5ec93578d78858feceb83844efb63f
Daniel Xu <dxu@dxuuu.xyz>
2020-09-18 17:05:35 -0700

    x.py: Run black formatter

--- x.py
+++ x.py
@@ -6,6 +6,7 @@ import pathlib
 import subprocess
 import sys
 
+
 def sh(cmd):
     try:
         subprocess.run(cmd, shell=True, check=True)
@@ -15,93 +16,91 @@ def sh(cmd):
 
 def cmd_build(args):
     if args.local:
-        sh('podman build -t btrfs-fuzz .')
+        sh("podman build -t btrfs-fuzz .")
     else:
-        sh('podman pull dxuu/btrfs-fuzz')
+        sh("podman pull dxuu/btrfs-fuzz")
 
 
 def cmd_run(args):
     print("Starting btrfs-fuzz")
 
-    c = ['podman run']
-    c.append('-it')
-    c.append('--privileged')
+    c = ["podman run"]
+    c.append("-it")
+    c.append("--privileged")
 
     if args.state_dir:
         c.append(f"-v {args.state_dir}:/state")
 
     if args.local:
-        c.append('localhost/btrfs-fuzz')
+        c.append("localhost/btrfs-fuzz")
     else:
-        c.append('dxuu/btrfs-fuzz')
+        c.append("dxuu/btrfs-fuzz")
 
-    sh(' '.join(c))
+    sh(" ".join(c))
 
 
 def cmd_shell(args):
-    c = ['podman run']
-    c.append('-it')
-    c.append('--privileged')
+    c = ["podman run"]
+    c.append("-it")
+    c.append("--privileged")
     c.append(f"-v {args.state_dir}:/state")
 
     if args.local:
-        c.append('localhost/btrfs-fuzz')
+        c.append("localhost/btrfs-fuzz")
     else:
-        c.append('dxuu/btrfs-fuzz')
+        c.append("dxuu/btrfs-fuzz")
 
-    c.append('--script-sh')
+    c.append("--script-sh")
     c.append('"')
-    c.append('echo core > /proc/sys/kernel/core_pattern')
-    c.append('/usr/local/bin/afl-fuzz')
-    c.append('-i /state/input')
-    c.append('-o /state/output')
-    c.append('-- /btrfs-fuzz/runner')
+    c.append("echo core > /proc/sys/kernel/core_pattern")
+    c.append("/usr/local/bin/afl-fuzz")
+    c.append("-i /state/input")
+    c.append("-o /state/output")
+    c.append("-- /btrfs-fuzz/runner")
     c.append('"')
 
-    sh(' '.join(c))
+    sh(" ".join(c))
 
 
 def main():
     parser = argparse.ArgumentParser(
-            prog='x',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-            '--local',
-            action='store_true',
-            help='Use local docker image')
+        prog="x", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--local", action="store_true", help="Use local docker image")
     parser.set_defaults(func=lambda _: parser.print_help())
 
-    subparsers = parser.add_subparsers(help='subcommands')
+    subparsers = parser.add_subparsers(help="subcommands")
 
-    build = subparsers.add_parser('build', help='build btrfs-fuzz components')
+    build = subparsers.add_parser("build", help="build btrfs-fuzz components")
     build.set_defaults(func=cmd_build)
 
-    run = subparsers.add_parser('run', help='run fuzzer')
+    run = subparsers.add_parser("run", help="run fuzzer")
     run.add_argument(
-            'state_dir',
-            type=str,
-            help='Shared state directory between host and VM, mounted in VM at '
-            '/state. The directory must contain `input` and `output` '
-            'subdirectories, with `input` containing initial test cases.')
+        "state_dir",
+        type=str,
+        help="Shared state directory between host and VM, mounted in VM at "
+        "/state. The directory must contain `input` and `output` "
+        "subdirectories, with `input` containing initial test cases.",
+    )
     run.set_defaults(func=cmd_run)
 
-    shell = subparsers.add_parser('shell', help='start shell in VM')
+    shell = subparsers.add_parser("shell", help="start shell in VM")
     shell.add_argument(
-            '-s',
-            '--state-dir',
-            type=str,
-            help='Shared state directory between host and VM, mounted in VM at /state')
+        "-s",
+        "--state-dir",
+        type=str,
+        help="Shared state directory between host and VM, mounted in VM at /state",
+    )
     shell.set_defaults(func=cmd_shell)
 
-    help = subparsers.add_parser(
-            'help',
-            help='print help')
+    help = subparsers.add_parser("help", help="print help")
     help.set_defaults(func=lambda _: parser.print_help())
 
     args = parser.parse_args()
     args.func(args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     proj_dir = pathlib.Path(__file__).parent
     os.chdir(proj_dir)
 
