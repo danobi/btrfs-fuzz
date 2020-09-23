096b9c3ba67c1c389060493c3db5c3c185498b7e
Daniel Xu <dxu@dxuuu.xyz>
2020-09-22 18:49:02 -0700

    mutator: Implement

--- src/mutator/src/lib.rs
+++ src/mutator/src/lib.rs
@@ -1,3 +1,29 @@
+use std::boxed::Box;
+use std::ptr;
+use std::slice;
+
+use anyhow::Result;
+use fuzzmutator::mutator::MutatorEngine;
+use rmp_serde::{decode::from_read_ref, Serializer};
+use serde::Serialize;
+
+use imgcompress::CompressedBtrfsImage;
+
+struct Mutator {
+    engine: MutatorEngine,
+    /// We'll return pointers to data in this buffer from `afl_custom_fuzz`
+    fuzz_buf: Vec<u8>,
+}
+
+impl Mutator {
+    fn new() -> Result<Self> {
+        Ok(Self {
+            engine: MutatorEngine::new()?,
+            fuzz_buf: Vec::new(),
+        })
+    }
+}
+
 /// Initialize this custom mutator
 ///
 /// @param[in] afl a pointer to the internal state object. Can be ignored for
@@ -8,8 +34,21 @@
 ///         There may be multiple instances of this mutator in one afl-fuzz run!
 ///         Return NULL on error.
 #[no_mangle]
-pub extern "C" fn afl_custom_init(_afl: *mut libc::c_void, _seed: libc::c_uint) -> *mut libc::c_void {
-    unimplemented!();
+pub extern "C" fn afl_custom_init(
+    _afl: *mut libc::c_void,
+    _seed: libc::c_uint,
+) -> *mut libc::c_void {
+    let mutator = match Mutator::new() {
+        Ok(m) => m,
+        Err(e) => {
+            println!("{}", e);
+            return ptr::null_mut();
+        }
+    };
+
+    let boxed = Box::new(mutator);
+
+    Box::into_raw(boxed) as *mut libc::c_void
 }
 
 /// Perform custom mutations on a given input
@@ -26,21 +65,44 @@ pub extern "C" fn afl_custom_init(_afl: *mut libc::c_void, _seed: libc::c_uint) 
 /// @return Size of the mutated output.
 #[no_mangle]
 pub extern "C" fn afl_custom_fuzz(
-    _data: *mut libc::c_void,
-    _buf: *mut u8,
-    _buf_size: u8,
-    _out_buf: *mut *mut u8,
+    data: *mut libc::c_void,
+    buf: *mut u8,
+    buf_size: u8,
+    out_buf: *mut *mut u8,
     _add_buf: *mut u8,
     _add_buf_size: libc::size_t,
-    _max_size: libc::size_t,
+    max_size: libc::size_t,
 ) -> libc::size_t {
-    unimplemented!();
+    let mutator = unsafe { &mut *(data as *mut Mutator) };
+
+    // Deserialize input
+    let serialized: &[u8] = unsafe { slice::from_raw_parts(buf, buf_size as usize) };
+    let mut deserialized: CompressedBtrfsImage =
+        from_read_ref(&serialized).expect("Failed to deserialize fuzzer input");
+
+    // Mutate payload (but don't touch the metadata)
+    mutator.engine.mutate(&mut deserialized.data);
+    // The engine shouldn't append any data but it's probably worthwhile to check again
+    assert!(deserialized.data.len() + deserialized.metadata.len() <= max_size);
+
+    // Serialize data again
+    mutator.fuzz_buf.clear(); // Does not affect capacity
+    deserialized
+        .serialize(&mut Serializer::new(&mut mutator.fuzz_buf))
+        .expect("Failed to serialize fuzzer input");
+    assert!(mutator.fuzz_buf.len() <= max_size);
+
+    // Yes, it's ok to hand out ref to the Vec we own. The API is designed this way
+    unsafe { out_buf.write(mutator.fuzz_buf.as_mut_ptr()) };
+
+    mutator.fuzz_buf.len()
 }
 
 /// Deinitialize everything
 ///
 /// @param data The data ptr from afl_custom_init
 #[no_mangle]
-pub extern "C" fn afl_custom_deinit(_data: *mut libc::c_void) {
-    unimplemented!();
+pub extern "C" fn afl_custom_deinit(data: *mut libc::c_void) {
+    // Reconstruct box and immediately drop to free resources
+    unsafe { Box::from_raw(data as *mut Mutator) };
 }
