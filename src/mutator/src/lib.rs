36115a2f55023efcb58c8b9264b26a9d74598d19
Daniel Xu <dxu@dxuuu.xyz>
2020-09-22 17:19:57 -0700

    mutator: Define other required functions

--- src/mutator/src/lib.rs
+++ src/mutator/src/lib.rs
@@ -1,5 +1,29 @@
-use libc;
+/// Initialize this custom mutator
+///
+/// @param[in] afl a pointer to the internal state object. Can be ignored for
+/// now.
+/// @param[in] seed A seed for this mutator - the same seed should always mutate
+/// in the same way.
+/// @return Pointer to the data object this custom mutator instance should use.
+///         There may be multiple instances of this mutator in one afl-fuzz run!
+///         Return NULL on error.
+#[no_mangle]
+pub extern "C" fn afl_custom_init(_afl: *mut libc::c_void, _seed: u32) -> *mut libc::c_void {
+    unimplemented!();
+}
 
+/// Perform custom mutations on a given input
+///
+/// @param[in] data pointer returned in afl_custom_init for this fuzz case
+/// @param[in] buf Pointer to input data to be mutated
+/// @param[in] buf_size Size of input data
+/// @param[out] out_buf the buffer we will work on. we can reuse *buf. NULL on
+/// error.
+/// @param[in] add_buf Buffer containing the additional test case
+/// @param[in] add_buf_size Size of the additional test case
+/// @param[in] max_size Maximum size of the mutated output. The mutation must not
+///     produce data larger than max_size.
+/// @return Size of the mutated output.
 #[no_mangle]
 pub extern "C" fn afl_custom_fuzz(
     _data: *mut libc::c_void,
@@ -10,5 +34,13 @@ pub extern "C" fn afl_custom_fuzz(
     _add_buf_size: libc::size_t,
     _max_size: libc::size_t,
 ) -> libc::size_t {
+    unimplemented!();
+}
+
+/// Deinitialize everything
+///
+/// @param data The data ptr from afl_custom_init
+#[no_mangle]
+pub extern "C" fn afl_custom_deinit(_data: *mut libc::c_void) {
     unimplemented!();
 }
