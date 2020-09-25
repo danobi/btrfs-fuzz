542d371e40557fe37f7aade23a180f8eb6c10b22
Daniel Xu <dxu@dxuuu.xyz>
2020-09-25 15:35:33 -0700

    scripts: Add validate_crashes.sh
    Helps with validating discovered crashes.

---
+++ scripts/validate_crashes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Script to see if discovered crashes can reproduce.
+#
+# Env vars:
+#     LOCAL: use `x.py --local`
+#
+# Example:
+#     LOCAL=1 ./validate_crashes.sh ./_state/output/crashes
+#
+
+set -eu
+
+if [[ "$#" != 1 ]]; then
+  echo 'Usage: validate_crashes.sh DIR' >> /dev/stderr
+fi
+
+if [[ -v LOCAL ]]; then
+  cmd="./x.py --local"
+else
+  cmd="./x.py"
+fi
+
+for f in "$1"/*; do
+  echo "Testing ${f}"
+  output=$($cmd repro --exit "$f")
+  if echo "$output" | grep -q FAILURE; then
+    echo -e "\tReproduced failure for ${f}"
+  fi
+done
